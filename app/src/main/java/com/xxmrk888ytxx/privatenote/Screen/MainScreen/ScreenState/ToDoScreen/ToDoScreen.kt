e673426e35853eb49353a1929c03909798cc43ec
xXMRK888YTXx <aksehchikov@gmail.com>
2022-08-12 12:07:00 +0300

    Добавление экрана для редактирования TODO экрана

--- app/src/main/java/com/xxmrk888ytxx/privatenote/Screen/MainScreen/ScreenState/ToDoScreen/ToDoScreen.kt
+++ app/src/main/java/com/xxmrk888ytxx/privatenote/Screen/MainScreen/ScreenState/ToDoScreen/ToDoScreen.kt
@@ -3,28 +3,29 @@ package com.xxmrk888ytxx.privatenote.Screen.MainScreen.ScreenState.ToDoScreen
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.InlineTextContent
-import androidx.compose.foundation.text.appendInlineContent
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.Placeholder
-import androidx.compose.ui.text.PlaceholderVerticalAlign
-import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.xxmrk888ytxx.privatenote.DB.Entity.ToDoItem
 import com.xxmrk888ytxx.privatenote.R
@@ -34,6 +35,9 @@ import com.xxmrk888ytxx.privatenote.ui.theme.*
 
 @Composable
 fun ToDoScreen(toDoViewModel: ToDoViewModel = hiltViewModel(),mainScreenController: MainScreenController) {
+    val state = remember {
+        toDoViewModel.getScreenState()
+    }
     LaunchedEffect(key1 = Unit, block = {
         toDoViewModel.setMainScreenController(mainScreenController)
     })
@@ -44,6 +48,112 @@ fun ToDoScreen(toDoViewModel: ToDoViewModel = hiltViewModel(),mainScreenControll
     ) {
         TopLabel()
         ToDoList(toDoViewModel)
+    }
+    if(state.value == ToDoScreenState.EditToDoDialog) {
+        EditToDoDialog(toDoViewModel)
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun EditToDoDialog(toDoViewModel: ToDoViewModel) {
+    val currentToDoImpotent = remember {
+        toDoViewModel.getIsCurrentEditableToDoImportantStatus()
+    }
+    val toDoEditItems = listOf(
+        ToDoEditItem(
+            icon = R.drawable.ic_priority_high,
+            activate = currentToDoImpotent.value,
+            activateColor = Color.Red.copy(0.9f)
+        ) {
+          toDoViewModel.changeImpotentStatus()
+        },
+        ToDoEditItem(
+            icon = R.drawable.ic_timer
+        ) {},
+        ToDoEditItem(
+            icon = R.drawable.ic_notifications
+        ) {},
+    )
+    val textField = remember {
+        toDoViewModel.dialogTextField
+    }
+    Dialog(onDismissRequest = { toDoViewModel.toDefaultMode()},
+        //properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Card(modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(20.dp),
+            backgroundColor = CardNoteColor
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                OutlinedTextField(value = textField.value, onValueChange = {
+                    if(it.length > 100) return@OutlinedTextField
+                    textField.value = it
+                },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(10.dp),
+                    singleLine = true,
+                    label = {
+                        Text(text = "Задача",
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = PrimaryFontColor
+                        )
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        textColor = PrimaryFontColor,
+                        backgroundColor = SearchColor,
+                        placeholderColor = PrimaryFontColor.copy(0.7f),
+                        focusedBorderColor = PrimaryFontColor,
+                        focusedLabelColor = PrimaryFontColor,
+                        cursorColor = PrimaryFontColor,
+                        unfocusedLabelColor = PrimaryFontColor.copy(0.6f)
+                    ),
+                    textStyle = TextStyle(
+                        fontWeight = FontWeight.Medium,
+                    ),
+                )
+                Row(modifier = Modifier.fillMaxWidth()) {
+                    LazyRow(
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        items(toDoEditItems) {
+                            val tint = if(it.activate) it.activateColor else it.deActivateColor
+                            Icon(painter = painterResource(it.icon),
+                                contentDescription = "",
+                                tint = tint,
+                                modifier = Modifier
+                                    .padding(5.dp)
+                                    .size(30.dp)
+                                    .clickable {
+                                        it.onClick()
+                                    }
+                            )
+                        }
+                    }
+                    OutlinedButton(
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = FloatingButtonColor,
+                            disabledBackgroundColor = FloatingButtonColor.copy(0.3f)
+                        ),
+                        enabled = textField.value.isNotEmpty(),
+                        onClick = {
+                            toDoViewModel.saveToDo()
+                        },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 5.dp, end = 5.dp),
+                        shape = RoundedCornerShape(80),
+                    ) {
+                        Text(text = stringResource(R.string.Save),
+                            color = PrimaryFontColor
+                        )
+                    }
+                }
+
+            }
+        }
     }
 }
 
@@ -59,7 +169,11 @@ fun ToDoList(toDoViewModel: ToDoViewModel) {
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(10.dp)
+                    .padding(10.dp).combinedClickable(
+                        onClick = {
+                            toDoViewModel.toEditToDoState(it)
+                        }
+                    )
                     .animateItemPlacement(),
                 shape = RoundedCornerShape(15),
                 backgroundColor = CardNoteColor.copy(0.3f)
@@ -73,7 +187,6 @@ fun ToDoList(toDoViewModel: ToDoViewModel) {
 @Composable
 fun ToDoItem(todo: ToDoItem, toDoViewModel: ToDoViewModel) {
     val fontColor = if(todo.isCompleted) PrimaryFontColor.copy(0.3f) else PrimaryFontColor
-    val textDecorate = if(todo.isCompleted) TextDecoration.LineThrough else TextDecoration.None
     Row(
         modifier = Modifier
             .padding(top = 10.dp, bottom = 10.dp)
@@ -104,7 +217,6 @@ fun ToDoItem(todo: ToDoItem, toDoViewModel: ToDoViewModel) {
             fontSize = 20.sp,
             fontWeight = FontWeight.Black,
             color = fontColor,
-            textDecoration = textDecorate,
             fontStyle = FontStyle.Italic,
         )
     }
--- app/src/main/java/com/xxmrk888ytxx/privatenote/Screen/MainScreen/ScreenState/ToDoScreen/ToDoViewModel.kt
+++ app/src/main/java/com/xxmrk888ytxx/privatenote/Screen/MainScreen/ScreenState/ToDoScreen/ToDoViewModel.kt
@@ -1,13 +1,12 @@
 package com.xxmrk888ytxx.privatenote.Screen.MainScreen.ScreenState.ToDoScreen
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.navigation.NavController
 import com.xxmrk888ytxx.privatenote.DB.Entity.ToDoItem
 import com.xxmrk888ytxx.privatenote.Repositories.ToDoRepository.ToDoRepository
 import com.xxmrk888ytxx.privatenote.Screen.MainScreen.MainScreenController
-import com.xxmrk888ytxx.privatenote.Screen.MainScreen.ScreenState.NoteState.NoteScreenMode
-import com.xxmrk888ytxx.privatenote.Screen.MultiUse.FloatButton.FloatButtonController
 import com.xxmrk888ytxx.privatenote.Utils.ShowToast
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
@@ -21,6 +20,49 @@ class ToDoViewModel @Inject constructor(
 ) : ViewModel() {
     private var mainScreenController: MainScreenController? = null
 
+    private val currentState:MutableState<ToDoScreenState> = mutableStateOf(ToDoScreenState.Default)
+
+    fun getScreenState() = currentState
+
+    val dialogTextField = mutableStateOf("")
+
+    private var currentEditableToDoId:Int = 0
+
+    private val isCurrentEditableToDoImportant = mutableStateOf(false)
+
+    private val currentToDoIsCompleted = mutableStateOf(false)
+
+    fun getIsCurrentEditableToDoImportantStatus() = isCurrentEditableToDoImportant
+
+    fun changeImpotentStatus() {
+        isCurrentEditableToDoImportant.value = !isCurrentEditableToDoImportant.value
+    }
+
+    fun toDefaultMode() {
+        currentState.value = ToDoScreenState.Default
+        clearEditableToDoInfo()
+    }
+
+    fun clearEditableToDoInfo() {
+        currentEditableToDoId = 0
+        dialogTextField.value = ""
+        isCurrentEditableToDoImportant.value = false
+        currentToDoIsCompleted.value = false
+    }
+
+    fun toEditToDoState(currentEditToDo:ToDoItem? = null) {
+        if(currentEditToDo != null) {
+            currentEditableToDoId = currentEditToDo.id
+            dialogTextField.value = currentEditToDo.todoText
+            isCurrentEditableToDoImportant.value = currentEditToDo.isImportant
+            currentToDoIsCompleted.value = currentEditToDo.isCompleted
+        }
+        else{
+            currentEditableToDoId = 0
+        }
+        currentState.value = ToDoScreenState.EditToDoDialog
+    }
+
     fun setMainScreenController(mainScreenController: MainScreenController?) {
         if(mainScreenController == null) return
         this.mainScreenController = mainScreenController
@@ -29,15 +71,7 @@ class ToDoViewModel @Inject constructor(
 
     private fun SetupFloatButtonOptions() {
         mainScreenController?.setFloatButtonOnClickListener {
-            viewModelScope.launch {
-                toDoRepository.insertToDo(
-                    ToDoItem(
-                        id = 0,
-                        todoText = "test",
-                        isImportant = false
-                    )
-                )
-            }
+            toEditToDoState()
         }
     }
 
@@ -49,5 +83,23 @@ class ToDoViewModel @Inject constructor(
         viewModelScope.launch {
             toDoRepository.changeMarkStatus(id,status)
         }
+    }
+
+    fun saveToDo() {
+        val currentId = currentEditableToDoId
+        val currentText = dialogTextField.value
+        val currentImportantState = isCurrentEditableToDoImportant.value
+        val currentIsComplited = currentToDoIsCompleted.value
+        viewModelScope.launch {
+            toDoRepository.insertToDo(
+                toDoItem = ToDoItem(
+                    id = currentId,
+                    todoText = currentText,
+                    isImportant = currentImportantState,
+                    isCompleted = currentIsComplited
+                )
+            )
+        }
+        toDefaultMode()
     }
 }
