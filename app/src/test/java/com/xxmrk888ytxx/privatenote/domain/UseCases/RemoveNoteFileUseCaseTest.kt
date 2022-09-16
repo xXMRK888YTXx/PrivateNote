a665128061adef42e906a9e2272fb52323eabdb9
xXMRK888YTXx <aksehchikov@gmail.com>
2022-09-16 20:17:30 +0300

    [Test] Написаны тесты для RemoveNoteFileUseCase

---
+++ app/src/test/java/com/xxmrk888ytxx/privatenote/domain/UseCases/RemoveNoteFileUseCaseTest.kt
@@ -0,0 +1,26 @@
+package com.xxmrk888ytxx.privatenote.domain.UseCases
+
+import com.xxmrk888ytxx.privatenote.domain.Repositories.AudioRepository.AudioRepositoryImpl
+import com.xxmrk888ytxx.privatenote.domain.Repositories.ImageRepository.ImageRepositoryImpl
+import io.mockk.coVerifySequence
+import io.mockk.mockk
+import io.mockk.verifySequence
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class RemoveNoteFileUseCaseTest {
+    @Test
+    fun test_removeNoteFiles__Invoke_This_Method_Expect_Remove_Note_Files() = runBlocking {
+        val imageRepository = mockk<ImageRepositoryImpl>(relaxed = true)
+        val audioRepository = mockk<AudioRepositoryImpl>(relaxed = true)
+        val noteId = 5
+        val useCase = RemoveNoteFileUseCaseImpl(imageRepository,audioRepository)
+
+        useCase.removeNoteFiles(noteId)
+
+        coVerifySequence {
+            imageRepository.clearNoteImages(noteId)
+            audioRepository.clearNoteAudios(noteId)
+        }
+    }
+}
