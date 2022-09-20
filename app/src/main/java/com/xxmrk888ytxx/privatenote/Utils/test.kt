e419b132a9b44710f485203fc57c5e89dd1cef94
xXMRK888YTXx <aksehchikov@gmail.com>
2022-09-20 19:43:30 +0300

    [Feature] Добавлен адаптер для парсаризации в JSON для ToDoItem

--- app/src/main/java/com/xxmrk888ytxx/privatenote/Utils/test.kt
+++ app/src/main/java/com/xxmrk888ytxx/privatenote/Utils/test.kt
@@ -7,6 +7,9 @@ import android.util.Base64
 import android.util.Base64.encode
 import androidx.annotation.RequiresApi
 import com.google.android.gms.common.util.Base64Utils.encode
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
 import com.xxmrk888ytxx.privatenote.MainActivity
 import com.xxmrk888ytxx.privatenote.data.Database.Entity.ToDoItem
 import kotlinx.coroutines.*
@@ -18,7 +21,19 @@ import javax.crypto.spec.DESKeySpec
 import javax.crypto.spec.SecretKeySpec
 
 fun main() = runBlocking {
-
+    val testClass = ToDoItem(
+        id = 8,
+        todoText = "parse to JSON",
+        isCompleted = false,
+        isImportant = true,
+        todoTime = 888888
+    )
+    val moshi: Moshi = Moshi.Builder().build()
+    val jsonAdapter: JsonAdapter<ToDoItem> = moshi.adapter(ToDoItem::class.java)
+    val json: String = jsonAdapter.toJson(testClass)
+    println(json)
+    val test =  jsonAdapter.fromJson(json)
+    println(test == testClass)
 }
 
 
--- app/src/main/java/com/xxmrk888ytxx/privatenote/data/Database/Entity/ToDoItem.kt
+++ app/src/main/java/com/xxmrk888ytxx/privatenote/data/Database/Entity/ToDoItem.kt
@@ -3,6 +3,7 @@ package com.xxmrk888ytxx.privatenote.data.Database.Entity
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.squareup.moshi.JsonClass
 
 @Entity(
     tableName = "TODO",
@@ -10,6 +11,7 @@ import androidx.room.PrimaryKey
         Index("id", unique = true)
     ],
 )
+@JsonClass(generateAdapter = true)
 data class ToDoItem(
     @PrimaryKey(autoGenerate = true) val id:Int = 0,
     val todoText:String,
